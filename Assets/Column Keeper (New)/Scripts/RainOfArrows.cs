8d2dd9cf7a3197915167ebba4e6b7db8da0c9a69
Horacio Valdes <horavaldesg@gmail.com>
2022-01-11 20:57:50 -0500

    Update RainOfArrows.cs

--- Assets/Column Keeper (New)/Scripts/RainOfArrows.cs
+++ Assets/Column Keeper (New)/Scripts/RainOfArrows.cs
@@ -26,7 +26,7 @@ public class RainOfArrows : MonoBehaviour
         Debug.Log(pos);
         //for (int i = 0; i < spawnAmount; i++)
         //{
-        GameObject arrowInstance = Instantiate(arrow,transform.position,Quaternion.identity);
+        GameObject arrowInstance = Instantiate(arrow,pos,Quaternion.identity);
             //arrowInstance.transform.position = pos;
             arrowInstance.transform.Rotate(90, 0, 0,Space.World);
             arrowInstance.GetComponent<Rigidbody>().freezeRotation = true;
