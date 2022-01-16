b06a4ffce50581284dce7f9f8042a202e24b26ee
Horacio Valdes <horavaldesg@gmail.com>
2022-01-15 23:09:42 -0500

    Update Arrow.cs

--- Assets/Column Keeper (New)/Scripts/Arrow.cs
+++ Assets/Column Keeper (New)/Scripts/Arrow.cs
@@ -144,7 +144,7 @@ public class Arrow : XRGrabInteractable
                         FireArrow fa = GetComponent<FireArrow>();
                         if (fa != null)
                         {
-                            Vector3 pos = new Vector3(transform.position.x, hit.collider.transform.position.y + 12, transform.position.z);
+                            Vector3 pos = new Vector3(lastPosition.x, hit.collider.transform.position.y + 12, lastPosition.z);
                             fa.Explode(hit.collider.transform.eulerAngles, pos);
                         }
 
