ed09c1daa4fe72321c9283896686ed58c197947d
Horacio Valdes <horavaldesg@gmail.com>
2022-01-15 23:14:30 -0500

    Fire arrow

--- Assets/Column Keeper (New)/Scripts/FireArrow.cs
+++ Assets/Column Keeper (New)/Scripts/FireArrow.cs
@@ -9,7 +9,7 @@ public class FireArrow : MonoBehaviour
 
     //[SerializeField]private GameObject explosionObj;
     private ParticleSystem ps;
-
+    [SerializeField] private GameObject explosionPos;
     private BlastWave bw;
     // Start is called before the first frame update
      void Start()
@@ -46,7 +46,7 @@ public class FireArrow : MonoBehaviour
     
     public void Explode(Vector3 rot, Vector3 pos)
     {
-        bw.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        explosionPos.transform.position = new Vector3(pos.x, pos.y, pos.z);
         bw.transform.eulerAngles =  Vector3.right * 90;
         StartCoroutine(bw.Blast());
         ps.Play();
