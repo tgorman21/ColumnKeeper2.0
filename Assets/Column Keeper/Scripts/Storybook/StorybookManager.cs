f769b56518a57329fdc9d8a08ab8e34b0d01feb6
Matt-Camerato <matt.camerato@gmail.com>
2022-02-05 18:01:14 -0500

    Storybook Update 1

---
+++ Assets/Column Keeper/Scripts/Storybook/StorybookManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StorybookManager : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Hand"))
+        {
+            anim.SetTrigger("open/close");
+        }
+    }
+}
