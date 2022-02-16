c999e671eaf8d2efb7fdff0220e613444b240dd1
Horacio Valdes <horavaldesg@gmail.com>
2022-02-16 14:07:39 -0500

    Destroy visuals effect

--- Assets/ColumnKeeper/Scripts/Arrows/MeteorShower.cs
+++ Assets/ColumnKeeper/Scripts/Arrows/MeteorShower.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.VFX;
 public class MeteorShower : MonoBehaviour
 {
     [SerializeField] GameObject meteorShower;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Shower(float damage, Vector3 pos)
     {
-        
+
         GameObject shower = Instantiate(meteorShower);
         shower.GetComponent<MeteorDamage>().damage = damage;
+        StartCoroutine(DestroyVisuals(shower));
         shower.transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+    IEnumerator DestroyVisuals(GameObject visualEffect)
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(visualEffect);
     }
 }
