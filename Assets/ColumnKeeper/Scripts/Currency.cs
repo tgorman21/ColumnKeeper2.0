59e1934e02f20917195aeb7627aa7935246fd254
Horacio Valdes <horavaldesg@gmail.com>
2022-02-16 21:17:00 -0500

    Currency

---
+++ Assets/ColumnKeeper/Scripts/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Currency : MonoBehaviour
+{
+    
+   
+    public void AddGold(float gold)
+    {
+        PlayerPrefs.SetFloat("Gold", gold);
+    }
+
+    public void AddDiamond(float diamond)
+    {
+        PlayerPrefs.SetFloat("Diamond", diamond);
+    }
+}
