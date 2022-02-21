f1407423dd85c4aadea660d53f9036c83e56f816
Matt-Camerato <matt.camerato@gmail.com>
2022-02-21 11:12:53 -0500

    UI Update for Main Menu

---
+++ Assets/ColumnKeeper/Scripts/UI/MainMenu_UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UIManager : MonoBehaviour
+{
+    public void StartLevel1()
+    {
+        Debug.Log("Load Level 1");
+    }
+
+    public void StartEndlessMode()
+    {
+        Debug.Log("Load EndlessMode");
+    }
+
+}
