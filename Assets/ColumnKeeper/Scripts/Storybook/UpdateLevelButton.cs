293a944d86e0d4c99ce66bbf8f2e5ad3b57de026
Matt-Camerato <matt.camerato@gmail.com>
2022-03-02 23:20:56 -0500

    Final fix

--- Assets/ColumnKeeper/Scripts/Storybook/UpdateLevelButton.cs
+++ Assets/ColumnKeeper/Scripts/Storybook/UpdateLevelButton.cs
@@ -5,15 +5,25 @@ using UnityEngine.UI;
 
 public class UpdateLevelButton : MonoBehaviour
 {
-    private void OnEnable()
+    [SerializeField] private int levelNum;
+
+    private void Awake()
     {
+        /*
         string levelName = gameObject.name;
         levelName.Remove(0, 5); //remove "Level" from button name
         levelName.Substring(0, levelName.LastIndexOf("_icon")); //remove "_icon" from button name
         int levelNum = int.Parse(levelName); //use string to find level #
-
+        */
         //check if this level is unlocked yet to determine if button is interactable or not
+        
+        
         if(Storybook.highestUnlockedLevel < levelNum) { GetComponent<Button>().interactable = false; }
         else { GetComponent<Button>().interactable = true; }
+
+
+        //DEV KEY
+        //Turn on all buttons for testing
+        GetComponent<Button>().interactable = true;
     }
 }
