884a621cf0d80879c3d4f8623b78b3a07b607b51
Matt-Camerato <matt.camerato@gmail.com>
2022-03-10 09:51:59 -0500

    Fixes for presentation

---
+++ Assets/ColumnKeeper/Scripts/UI/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform cam;
+
+    private void Update()
+    {
+        transform.rotation = cam.rotation;
+    }
+}
