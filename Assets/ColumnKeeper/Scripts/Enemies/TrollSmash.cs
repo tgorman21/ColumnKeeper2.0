ae3d91f83bed182e1cbd4f80fd4611dbce017ebc
tgorman21 <72713859+tgorman21@users.noreply.github.com>
2022-03-19 02:52:51 -0400

    Troll Death Animation

--- Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
+++ Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class TrollSmash : MonoBehaviour
 {
     public ParticleSystem Smash;
+    public ParticleSystem TrollDie;
     void SmashParticles()
     {
         Smash.Play();
      }
+    void DieParticles()
+    {
+        TrollDie.Play();
+    }
 }
