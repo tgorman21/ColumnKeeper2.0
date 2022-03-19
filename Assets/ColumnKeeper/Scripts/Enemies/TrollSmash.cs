1fd03f78d0c9d44cfe0b7615fdf5f41496dbeaac
tgorman21 <72713859+tgorman21@users.noreply.github.com>
2022-03-19 02:24:36 -0400

    Troll Attack Anim

---
+++ Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollSmash : MonoBehaviour
+{
+    public ParticleSystem Smash;
+    void SmashParticles()
+    {
+        Smash.Play();
+     }
+}
