3a1627815c3ff164aafd2fdaa2f59e2282347bea
tgorman21 <72713859+tgorman21@users.noreply.github.com>
2022-04-15 19:46:28 -0400

    Troll Hp and text fix

--- Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
+++ Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
@@ -6,6 +6,29 @@ public class TrollSmash : MonoBehaviour
 {
     public ParticleSystem Smash;
     public ParticleSystem TrollDie;
+    private float HP;
+    private float t = 0;
+    private void Start()
+    {
+        HP = gameObject.GetComponent<Enemy>().health;
+    }
+    private void Update()
+    {
+        if (HP >= 200) return;
+
+        t += Time.deltaTime;
+
+        if(t >= 1)
+        {
+            t = 0;
+            HP += 3;
+            Debug.Log(HP);
+                Debug.Log(3);
+        }
+    }
+
+
+
     void SmashParticles()
     {
         Smash.Play();
