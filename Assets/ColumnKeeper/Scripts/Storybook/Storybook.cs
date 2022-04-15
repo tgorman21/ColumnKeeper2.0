a5df16b61906b87d6b0b83094dff81b316547dc2
Matt-Camerato <matt.camerato@gmail.com>
2022-04-15 18:55:49 -0400

    goodbye storybook part 1

--- Assets/ColumnKeeper/Scripts/Storybook/Storybook.cs
+++ Assets/ColumnKeeper/Scripts/Storybook/Storybook.cs
@@ -13,12 +13,11 @@ public class Storybook : MonoBehaviour
     [SerializeField] private ContinuousMoveProviderBase playerMovement;
 
     public static int highestUnlockedLevel = 1;
-    private static bool playPostDialog = false;
-    private static string[] preDialog;
-    private static string[] postDialog;
 
     private static int levelNum;
     private int lineNum;
+
+    private string[] dialog;
 
     private Animator anim;
     private AudioSource audioSource;
@@ -27,24 +26,13 @@ public class Storybook : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
-        
-        if (playPostDialog) //when scene starts, check if player is returning from level to trigger post dialog 
-        {
-            //trigger post dialog animation
-            anim.SetTrigger("postdialog");
 
-            //freeze movement of player
-            TogglePlayerMovement(); 
+        //move XRRig to be in front of the storybook
+        Vector3 pos = new Vector3(3.5f, 0.6f, -0.65f);
+        XRRig.position = pos;
 
-            //move XRRig to be in front of the storybook
-            Vector3 pos = new Vector3(3.5f, 0.6f, -0.65f);
-            XRRig.position = pos;
-
-            //unlock next level if player just completed highest unlocked level
-            if(levelNum == highestUnlockedLevel) { highestUnlockedLevel++; }
-
-            lineNum = 1; //HOT FIX: I just made this equal to 1 instead of 0 to skip the first entry of the end dialog (due to it being an empty string when read from text file)
-        }
+        //unlock next level if player just completed highest unlocked level
+        if(levelNum == highestUnlockedLevel) { highestUnlockedLevel++; }
     }
 
     public void StoryMode() => anim.SetTrigger("storymode");
@@ -79,32 +67,24 @@ public class Storybook : MonoBehaviour
         //var fileContents = sr.ReadToEnd();
         //sr.Close();
 
-        string[] PreAndPost = script.Split("#"[0]);
-
         char[] newLine = { '\n' };
-        preDialog = PreAndPost[0].Split(newLine, System.StringSplitOptions.RemoveEmptyEntries);
-        postDialog = PreAndPost[1].Split(newLine, System.StringSplitOptions.RemoveEmptyEntries);
+        dialog = script.Split(newLine, System.StringSplitOptions.RemoveEmptyEntries);
 
         levelNum = _levelNum;
         lineNum = 0;
-
-        TogglePlayerMovement(); //freeze movement of player for remainder of time in scene
     }
 
-    public void PreDialog() => StartCoroutine(ProgressDialog(preDialog));
-    public void PostDialog() => StartCoroutine(ProgressDialog(postDialog));
+    public void TriggerDialog() => StartCoroutine(ProgressDialog(dialog));
 
     private IEnumerator ProgressDialog(string[] dialog)
     {
-        if (!dialog[lineNum].Contains(":")) lineNum++; //TEST FIX: This should skip the first blank entry in the post dialog
-
         string[] currentLine = dialog[lineNum].Split(":"[0]);
         string character = currentLine[0];
         string[] scriptAndDuration = currentLine[1].Split("-"[0]);
         string script = scriptAndDuration[0];
         int duration = int.Parse(scriptAndDuration[1]);
 
-        DisplayCharacter(character, duration);
+        //DisplayCharacter(character, duration);
         dialogText.text = script;
         lineNum++;
 
@@ -116,21 +96,9 @@ public class Storybook : MonoBehaviour
 
     private void EndDialog()
     {
-        if (!playPostDialog) //end of predialog
-        {
-            string sceneName = "Stage_" + levelNum.ToString();
-            CustomSceneManager.instance.GoToScene(sceneName);
-        }
-        else //end of postdialog
-        {
-            TogglePlayerMovement(); //unfreeze player
-
-            anim.SetTrigger("enddialog"); //trigger animation to reset book back to level select mode
-        }
-        playPostDialog = !playPostDialog;
+        string sceneName = "Stage_" + levelNum.ToString();
+        CustomSceneManager.instance.GoToScene(sceneName);
     }
-
-    private void TogglePlayerMovement() => playerMovement.enabled = !playerMovement.enabled;
 
     private void DisplayCharacter(string character, int duration)
     {
