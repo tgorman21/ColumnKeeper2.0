21b46ffe725dce079c16e1add7f6998fec2152ec
Horacio Valdes <horavaldesg@gmail.com>
2022-04-20 09:40:37 -0400

    Working Healing Effect

--- Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
+++ Assets/ColumnKeeper/Scripts/Enemies/TrollSmash.cs
@@ -9,7 +9,7 @@ public class TrollSmash : MonoBehaviour
     [SerializeField] private ParticleSystem healingParticles;
 
     private float HP;
-    private float t = 0;
+    private float _t = 0;
     private float initialHp;
 
     private void Start()
@@ -22,30 +22,45 @@ public class TrollSmash : MonoBehaviour
     {
         HP = gameObject.GetComponent<Enemy>().health;
         if (HP >= initialHp) return;
-
-        t += Time.deltaTime;
-        Debug.Log(t);
-        if (t >= 15)
+        
+        
+        Debug.Log(_t);
+        if (StartHealing(_t))
         {
             StartCoroutine(Heal());
-
+            return;
         }
+        _t += Time.deltaTime;
+    }
+    private bool StartHealing(float t)
+    {
+
+        if (t >= 15)
+        {
+            return true;
+        }
+        else
+            return false;
+
     }
     IEnumerator Heal()
     {
         HealingEffect(true);
         gameObject.GetComponent<Enemy>().health = Mathf.Lerp(HP, HP + 5, Time.deltaTime);//Increases health by 5
         yield return new WaitForSeconds(5);
-        t = 0;
+        _t = 0;
         HealingEffect(false);
 
     }
-    void HealingEffect(bool state)
+    private void HealingEffect(bool state)
     {
-        if (state)
-            healingParticles.Play();
-        else if(!state)
-            healingParticles.Stop();
+        switch (state)
+        {
+            case true: if(!healingParticles.isPlaying) healingParticles.Play();
+                break;
+            case false: healingParticles.Stop();
+                break;
+        }
     }
 
     void SmashParticles()
