318eb77ba12e565525aeb4285c8730fb63b1c96b
Horacio Valdes <horavaldesg@gmail.com>
2022-04-21 15:45:09 -0400

    Fire effect

--- Assets/ColumnKeeper/Scripts/Arrows/AddFire.cs
+++ Assets/ColumnKeeper/Scripts/Arrows/AddFire.cs
@@ -21,10 +21,11 @@ public class AddFire : MonoBehaviour
     {
         if (other.CompareTag("Arrow"))
         {
-            Transform tip = other.GetComponent<Arrow>().tip;
-            GameObject arrowFire = Instantiate(fire, other.GetComponent<Arrow>().tip);
-            arrowFire.transform.position = new Vector3(tip.transform.position.x + 0.25f, tip.transform.position.y - 0.25f, tip.transform.position.z);
-            arrowFire.transform.parent = other.transform;
+            GameObject fireEffect = other.transform.GetChild(0).GetChild(0).gameObject;
+            for(int i =0; i<fireEffect.transform.childCount; i++)
+            {
+                fireEffect.transform.GetChild(i).gameObject.SetActive(true);
+            }
         }
     }
 
