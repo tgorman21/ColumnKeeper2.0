0262e1315153a3f14169cef3501332496f0df55e
Horacio Valdes <horavaldesg@gmail.com>
2022-04-21 15:45:58 -0400

    Fire effect

--- Assets/ColumnKeeper/Scripts/Arrows/AddFire.cs
+++ Assets/ColumnKeeper/Scripts/Arrows/AddFire.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AddFire : MonoBehaviour
 {
-    public GameObject fire;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +20,7 @@ public class AddFire : MonoBehaviour
     {
         if (other.CompareTag("Arrow"))
         {
-            GameObject fireEffect = other.transform.GetChild(0).GetChild(0).gameObject;
+            GameObject fireEffect = other.transform.GetChild(0).GetChild(1).gameObject;
             for(int i =0; i<fireEffect.transform.childCount; i++)
             {
                 fireEffect.transform.GetChild(i).gameObject.SetActive(true);
