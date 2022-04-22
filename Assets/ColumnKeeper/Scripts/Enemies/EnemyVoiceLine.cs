8b24a16ad45f45b6358fab9b23d355d5cc6b345b
Horacio Valdes <horavaldesg@gmail.com>
2022-04-22 12:16:31 -0400

    Enemy Voice Lines

---
+++ Assets/ColumnKeeper/Scripts/Enemies/EnemyVoiceLine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVoiceLine : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField] List<AudioClip> voiceLines = null;
+    [SerializeField] private float voiceLineRate;
+    private bool play;
+    private float t;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        play = false;
+    }
+
+    private void Update()
+    {
+        t += Time.deltaTime;
+        if (t > voiceLineRate && !play)
+        {
+            PlayVoiceLine(voiceLines);
+        }
+    }
+
+    //Plays the voice line
+    private void PlayVoiceLine(List<AudioClip> clips)
+    {
+        play = true;
+        t = 0;
+        int r = Randomize();
+        if(!audioSource.isPlaying) audioSource.PlayOneShot(clips[r]);
+        RemoveVoiceLine(clips, r);
+    }
+
+    //Randomizes Next audio clip to play
+    private int Randomize()
+    {
+        int randomNumber = Random.Range(0, voiceLines.Count);
+        return randomNumber;
+    }
+
+    //Removes that voice line from list 
+    private void RemoveVoiceLine(List<AudioClip> newList, int index)
+    {
+        newList.Remove(newList[index]);
+        voiceLines = newList;
+
+    }
+}
